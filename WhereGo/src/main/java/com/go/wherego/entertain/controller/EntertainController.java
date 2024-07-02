1d512f2d8f18353592489098112ec7df6954df89
coocoosumchicken <bria0130@gmail.com>
2024-07-02 11:39:01 +0900

    0702 11:38 룰렛 완료

--- WhereGo/src/main/java/com/go/wherego/entertain/controller/EntertainController.java
+++ WhereGo/src/main/java/com/go/wherego/entertain/controller/EntertainController.java
@@ -1,6 +1,9 @@
 package com.go.wherego.entertain.controller;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,65 +18,60 @@ import com.go.wherego.member.model.vo.Member;
 import com.go.wherego.trip.model.vo.Trip;
 import com.google.gson.Gson;
 
-
-
 @Controller
 public class EntertainController {
-	
+
 	@Autowired
 	private EntertainService entertainService;
-	
+
 	@Autowired
 	private MemberService memberService;
-	
+
 	@RequestMapping("worldcup.en")
 	public String startWorldCup(Model model) {
 		ArrayList<Trip> list = entertainService.getTop100();
-		  //System.out.println("처 음"+list); 
-		  model.addAttribute("list",new Gson().toJson(list));
+		// System.out.println("처 음"+list);
+		model.addAttribute("list", new Gson().toJson(list));
 		return "entertain/worldcup";
 	}
-	
+
 	@RequestMapping("rullet.en")
 	public String startRullet(Model model) {
-		/*
-		 * ArrayList<Trip> list = entertainService.getTop100();
-		 * System.out.println("처 음"+list); model.addAttribute("list",new
-		 * Gson().toJson(list));
-		 */
-	return "entertain/rullet";
+		ArrayList<Trip> list = entertainService.getTop100();
+		ArrayList<Trip> ranList=makeRandom(list);
+		model.addAttribute("list", new Gson().toJson(ranList));
+		System.out.println(ranList);
+		return "entertain/realRullet";
 	}
-	
+
 	/*
-	 * @RequestMapping("passGang") 
-	 * public String selectGang(Model model, String gang) { 
-	 * ArrayList<Trip> list = entertainService.getTop100();
-	 * System.out.println("처 음"+list); 
-	 * model.addAttribute("list",new Gson().toJson(list));
-	 * }
+	 * @RequestMapping("passGang") public String selectGang(Model model, String
+	 * gang) { ArrayList<Trip> list = entertainService.getTop100();
+	 * System.out.println("처 음"+list); model.addAttribute("list",new
+	 * Gson().toJson(list)); }
 	 */
 
-	// 가져온 Trip 랜덤으로 섞어서 입력한 개수만큼 뽑아오기 
-//	public static ArrayList<Trip> makeRandom(ArrayList<Trip> list, int gang) {
-//
-//        Random random = new Random();
-//        Set<Trip> selectedElements = new HashSet<>();
-//        ArrayList<Trip> resultList = new ArrayList<>();
-//
-//        while (selectedElements.size() < gang) {
-//            int randomIndex = random.nextInt(list.size());
-//            Trip trip = list.get(randomIndex);
-//            if (selectedElements.add(trip)) {
-//                resultList.add(trip);
-//            }
-//        }
-//        
-//        return resultList;
-//    }
-	
+	 //가져온 Trip 랜덤으로 섞어서 16개 가져오기
+	public static ArrayList<Trip> makeRandom(ArrayList<Trip> list) {
+
+        Random random = new Random();
+        Set<Trip> selectedElements = new HashSet<>();
+        ArrayList<Trip> resultList = new ArrayList<>();
+
+        while (selectedElements.size() < 12) {
+            int randomIndex = random.nextInt(list.size());
+            Trip trip = list.get(randomIndex);
+            if (selectedElements.add(trip)) {
+                resultList.add(trip);
+            }
+        }
+        
+        return resultList;
+    }
+
 	@PostMapping("wcResult.en")
-	public String insertWcResult(String userId,String winnerName,String check,Model model) {
-		int flag=Integer.parseInt(check);
+	public String insertWcResult(String userId, String winnerName, String check, Model model) {
+		int flag = Integer.parseInt(check);
 		/*
 		 * if(userId==null) { if(flag==0) { return "entertain/index"; }else { return
 		 * "entertain/ranking" ; } }
@@ -89,20 +87,15 @@ public class EntertainController {
 		wc.setGender(m.getGender());
 		System.out.println(wc);
 		entertainService.insertWCResult(wc);
-		if(flag==0) {
-			System.out.println("결승전 끝, 우승자는 : "+winnerName+" check : "+check+" 다시하기");
-			ArrayList<Trip>  list = entertainService.getTop100();
-			model.addAttribute("list",new Gson().toJson(list));
+		if (flag == 0) {
+			System.out.println("결승전 끝, 우승자는 : " + winnerName + " check : " + check + " 다시하기");
+			ArrayList<Trip> list = entertainService.getTop100();
+			model.addAttribute("list", new Gson().toJson(list));
 			return "entertain/worldcup";
-		}else {
-			System.out.println("결승전 끝, 우승자는 : "+winnerName+" check : "+check+" 랭킹보기");
+		} else {
+			System.out.println("결승전 끝, 우승자는 : " + winnerName + " check : " + check + " 랭킹보기");
 			return "entertain/ranking";
 		}
 	}
-	
-	
-	
-	
-	
-	
+
 }
